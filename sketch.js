f1e1c46aa23360bf8f7b0e6d90b50a08ee982135
kennahi <khaldoun.ennahi@gmail.com>
2021-11-22 23:21:12 +0100

    added button visual feedback

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ let currentColor = '#000000';
 let color = 1;
 let gridToggle = 1;
 let realColor;
+let firstButton = 1;
+let secondButton = 0;
 
 let i = 0;
 for (i; i < 16; i++) {
@@ -25,22 +27,28 @@ function setCurrentColor(newColor) {
 //goes through the function whenever there is a change in [coloring]
 coloring.onchange = (e) => setCurrentColor(e.target.value);
 
-function colorWhite(div) {
-    div.classList.remove("blacked");
-}
-
 eraser.addEventListener('click', () => {
     color = 0;
+    eraser.classList.add('blacked');
+    draw.classList.remove('blacked');
+    clear.classList.remove('blacked');
+    gridder.classList.remove('blacked');
 });
 
 draw.addEventListener('click', () => {
     color = 1;
+    draw.classList.add('blacked');
+    gridder.classList.remove('blacked');
+    clear.classList.remove('blacked');
+    eraser.classList.remove('blacked');
 });
 
 let divs = document.querySelectorAll(".gridSquare");
 
 gridder.addEventListener('click', () => {
-    console.log(divs.length);
+    draw.classList.remove('blacked');
+    clear.classList.remove('blacked');
+    eraser.classList.remove('blacked');
     if (gridToggle == 1) {
         let len = divs.length;
         for (let i = 0; i < len; i++) {
@@ -55,13 +63,18 @@ gridder.addEventListener('click', () => {
         }
         gridToggle = 1;
     }
+    gridder.classList.add('blacked');
 });
 
 clear.addEventListener('click', () => {
+    draw.classList.remove('blacked');
+    gridder.classList.remove('blacked');
+    eraser.classList.remove('blacked');
     let len = divs.length;
     for (let i = 0; i < len; i++) {
         divs[i].style.backgroundColor = '#FFFFFF';
     }
+    clear.classList.add('blacked');
 })
 
 function changeColor(e) {
@@ -73,3 +86,5 @@ function changeColor(e) {
 }
 
 divs.forEach(div => div.addEventListener('mouseover', changeColor));
+
+draw.classList.add('blacked');
